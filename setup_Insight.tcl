b3b404816e8018dbc28a678e89636876846d1789
EBengoechea <60866+ebengoechea@users.noreply.github.com>
2021-02-14 00:34:49 +0100

    Initial commit. Spinned-off from DYE DSx plugin code.

---
+++ setup_Insight.tcl
@@ -0,0 +1,42 @@
+# Sets up DGUI aspect variables for the Insight skin.
+
+proc ::plugins::DGUI::setup_aspect_Insight {} {
+	# See skins/Insight/skin.tcl
+	variable page_bg_image "[plugin_directory]/DGUI/bg_Insight.jpg"
+	variable bg_color "#ffffff"
+	variable font_color "#7f879a"
+	variable page_title_color "#7f879a"
+	variable remark_color orange
+	variable error_color red
+	variable disabled_color "#ddd"
+	variable highlight_color $font_color
+	variable insert_bg_color orange
+	variable font "Helv"
+	variable font_size 7
+	variable header_font "Helv_bold"
+	variable header_font_size 11
+	variable section_font_size 10	
+	variable entry_relief flat
+	
+	variable button_font "Helv_bold"
+	variable button_font_fill "#ffffff"
+	variable button_fill "#c0c5e3"
+	
+	variable listbox_relief flat
+	variable listbox_bwidth 0
+	variable listbox_fg $font_color
+	variable listbox_sfg black
+	variable listbox_bg $bg_color
+	variable listbox_sbg "#c0c4e1"
+	variable listbox_sbwidth 0
+	variable listbox_hthickness 1
+	variable listbox_hcolor $font_color
+
+	variable scrollbar_bwidth 0
+	variable scrollbar_relief flat		
+	variable scrollbar_bg "#d3dbf3"
+	variable scrollbar_fg "#FFFFFF"
+	variable scrollbar_troughcolor "#f7f6fa"
+	variable scrollbar_hthickness 0
+}
+
